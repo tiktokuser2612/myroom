51fc53bc22a39ae9d684960ea2242f42b522c908
amarbahadurraj <tiktokuser2612@gmail.com>
2022-06-18 10:25:05 +0530

    Add React

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+// import User from './User';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <User />
+      <User />
+    </div>
+  );
+}
+
+function User(){
+  return (
+      <div>
+<h1>User Page </h1>
+      <h1>User Component </h1>
+      </div>
+      
+  )
+}
+     
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,11 @@
+function User(){
+    return (
+        <div>
+<h1>User Page </h1>
+        <h1>User Component </h1>
+        </div>
+        
+    )
+}
+
+export default User;
